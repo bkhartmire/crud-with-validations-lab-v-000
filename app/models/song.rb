d3b71d0fef4c14fa05607e34d5ccbd48d7070d8b
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-28 02:15:05 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true, uniqueness: {scope: :artist_name && :release_year}
   validates :released, inclusion: { in: %w(true, false) }
-  validates :release_year, numericality: true
+  validates :release_year, numericality: true, presence: true, if: Proc.new {|a| a.released? }
   validates :artist_name, presence: true
 
 end
