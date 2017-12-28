c1ac55e8907f746923339183c702e6f780f37e9a
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-28 02:33:23 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :title, uniqueness: {scope: [:artist_name, :release_year]}, message: "Cannot be repeated by the same artist in the same year."
+  validates :title, uniqueness: {scope: [:artist_name, :release_year], message: "A song cannot be repeated by the same artist in the same year."}
   validates :released, inclusion: { in: %w(true, false) }
   validates :release_year, numericality: {less_than_or_equal_to: Time.now.year}, presence: true, if: Proc.new {|a| a.released? }
   validates :artist_name, presence: true
